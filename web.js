2b82e200d59394502fd8a2aa39bb25fffc3b64ca
Amil <amilmartin@gmail.com>
2011-12-13 01:10:26 -0500

    Added web.js

--- web.js
+++ web.js
@@ -31,5 +31,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(80);
+app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
